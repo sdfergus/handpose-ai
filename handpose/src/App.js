96f7d25a395a77b30c0892dcfbf8a438a4973cdb
Shandira Ferguson <shandira.ferguson@gmail.com>
2023-07-26 13:51:36 -0700

    webcam + canvas successfully running in DOM

--- handpose/src/App.js
+++ handpose/src/App.js
@@ -1,7 +1,7 @@
 /**
  * TODO: 
  * 1. Install dependencies = tensorflow.js, hand pose model, react webcam - DONE
- * 2. Import dependencies
+ * 2. Import dependencies - DONE
  * 3. Setup webcam and canvas
  * 4. Define references to those
  * 5. Load handpose
@@ -10,28 +10,57 @@
  * 8. Draw functions
  */
 
-import logo from './logo.svg';
+import React, {useRef} from 'react';
+import * as tf from '@tensorflow/tfjs';
+import * as handpose from '@tensorflow-models/handpose';
+import Webcam from 'react-webcam'; //to use desktop webcam
 import './App.css';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam 
+          ref={webcamRef}
+          style={{
+            position:'absolute', 
+            marginLeft: 'auto',
+            marginRight: 'auto', 
+            left: 0, 
+            right: 0, 
+            textAlign:'center',
+            zindex:9,
+            width:640,
+            height:480
+          }}
+        />
+        <canvas 
+          ref={canvasRef}
+          style={{
+            position:'absolute', 
+            marginLeft: 'auto',
+            marginRight: 'auto', 
+            left: 0, 
+            right: 0, 
+            textAlign:'center',
+            zindex:9,
+            width:640,
+            height:480
+          }}
+        />
       </header>
     </div>
   );
 }
 
 export default App;
+
+/**
+ * Notes: 
+ * 
+ * useRef = allows to have references to the elements within the webpage or DOM 
+ * 
+ */
